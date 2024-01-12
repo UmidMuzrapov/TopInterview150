056340f79d016c0218d53147cf64931d08c0fbf1
Umid Muzrapov <63548446+UmidMuzrapov@users.noreply.github.com>
2024-01-12 12:29:38 -0700

    Solve Ransom Note

---
+++ hashmap/ransom_note/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        char[] rArr = new char[26];
+        char[] mArr = new char[26];
+
+        mapSpe(rArr, ransomNote);
+        mapSpe(mArr, magazine);
+
+        for (int i = 0; i<26; i++){
+            if (mArr[i] < rArr[i]) return false;
+        }
+
+        return true;
+    }
+
+    private void mapSpe(char[] m, String str){
+        for (char ch: str.toCharArray()){
+            m[ch - 'a'] += 1 ;
+        }
+    }
+}
