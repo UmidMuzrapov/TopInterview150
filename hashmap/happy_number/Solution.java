8fcb511e4e88b63f2941f5703fa05dae062a43c7
U Muzrapov <umimuzra.gamil.com>
2024-01-13 16:03:34 -0700

    solve happt number problem

---
+++ hashmap/happy_number/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isHappy(int n) {
+        if (n==1) return true;
+
+        while (true && n>6){
+            String str = String.valueOf(n);
+            int x = 0;
+            for (int i = 0; i < str.length(); i++){
+                x+=(str.charAt(i) - '0')*(str.charAt(i) - '0');
+            }
+
+            if (x==1) return true;
+            else n = x; 
+        }
+
+        return false;
+    }
+}
