c9debb35f22546880477ed077fa8bea59bcfc053
Umid Muzrapov <63548446+UmidMuzrapov@users.noreply.github.com>
2024-01-15 15:32:20 -0700

    Solve duplicate 2 problem

---
+++ hashmap/contains_duplicate_2/Solution.java
@@ -0,0 +1,24 @@
+/**
+    map that maps number to its index
+    for num in nums
+        if map contains number
+            if j - i <= k return true
+        put num, j
+ */
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> map =  new HashMap<Integer, Integer>();
+        
+        for (int j=0; j < nums.length; j++){
+            if (map.containsKey(nums[j])){
+                if (j - map.get(nums[j]) <=k ){
+                    return true;
+                }
+            }
+
+            map.put(nums[j], j);
+        }
+
+        return false;
+    }
+}
