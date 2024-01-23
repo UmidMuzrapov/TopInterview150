28b34121b2f8372f6c4619208b94cbeb308753ab
Umid Muzrapov <63548446+UmidMuzrapov@users.noreply.github.com>
2024-01-23 13:56:47 -0700

    Solve minimum absolute difference

---
+++ binary_search_tree/minimum_absolute_difference/Solution.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int getMinimumDifference(TreeNode root) {
+      ArrayList<Integer> list = new ArrayList<Integer>();
+      int min = Integer.MAX_VALUE;
+      recurse(root, list);
+      
+      for (int i=1; i<list.size(); i++){
+         if (list.get(i) - list.get(i-1) < min){
+             min = list.get(i) - list.get(i-1);
+         }
+      }
+
+      return min;
+    }
+
+    private void recurse(TreeNode root, ArrayList<Integer> list){
+        if (root==null) return;
+        recurse(root.left, list);
+        list.add(root.val);
+        recurse(root.right, list);
+    }
+}
